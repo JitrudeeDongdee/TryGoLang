292a9db51b680a834db5f226d3175678626e8945
JitreudeeDoungdee <s6301012620022@email.kmutnb.ac.th>
2024-01-15 03:19:24 +0700

    grade not finish

--- grad.go
+++ grad.go
@@ -1,12 +1,21 @@
 package main
 
-func (score int) int {
-	var Grade []string{'A','B','C','D'}
-	fmt.Println(Grade)
-	// 80 7 6 5
-	return score
-}
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	score(10)
+	var Grade []string
+	Grade = []string{"F", "D", "C", "B", "A"}
+	fmt.Println(Grade)
+	score := 80
+	sl := math.Ceil(float64(score)/20) - 1
+	fmt.Println(sl)
+	// s := 4
+	fmt.Println(Grade[int(sl)])
+	// fmt.Println(math.Ceil(float64(score) / 10))
+	// var buffer int
+	// fmt.Println("buffer",score(10))
+	// fmt.Println("Hi")
 }
